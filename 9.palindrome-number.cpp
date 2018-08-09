dcaecdb84d5a0f58bea32f37b6c0e17a61291dc8
ChunYoLin <r04922108@csie.ntu.edu.tw>
2018-08-09 20:25:36 +0800

    update 9.palindrome-number.cpp

--- 9.palindrome-number.cpp
+++ 9.palindrome-number.cpp
@@ -2,22 +2,15 @@ class Solution {
     public:
         bool isPalindrome(int x) 
         {
+            int ori_x = x;
             if (x < 0)
                 return false;
-            vector<char> num_list;
-            while (x/10 > 0)
+            int reverse_x = 0;
+            while (x > 0)
             {
-                int num = x%10;
-                char num_c = num + '0';
-                num_list.push_back(num_c);
-                x /= 10;
+                reverse_x = reverse_x*10 + x%10; 
+                x/=10;
             }
-            num_list.push_back((x%10)+'0');
-            for (int i = 0; i < num_list.size()/2; i++)
-            {
-                if (num_list[i] != num_list[num_list.size()-i-1])
-                    return false;
-            }
-            return true;
+            return ori_x == reverse_x;
         }
 };
