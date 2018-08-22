b14bdd3f0eafc492b81888617e054b931a9aea32
ChunYoLin <r04922108@csie.ntu.edu.tw>
2018-08-22 22:48:52 +0800

    update 28.implement-strstr.cpp

---
+++ 28.implement-strstr.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+        int strStr(string haystack, string needle)
+        {
+            if (needle.size() == 0)
+                return 0;
+            int h_ptr = 0, n_ptr = 0, ans = -1;
+            while (h_ptr < haystack.size())
+            {
+                while (h_ptr < haystack.size() && n_ptr < needle.size() && haystack[h_ptr] == needle[n_ptr])
+                {
+                    if (n_ptr == 0)
+                        ans = h_ptr;
+                    h_ptr++;
+                    n_ptr++;
+                }
+                std::cout << n_ptr;
+                if (n_ptr == needle.size())
+                    return ans;
+                else
+                {
+                    h_ptr -= n_ptr;
+                    n_ptr = 0;
+                    ans = -1;
+                    h_ptr++;
+                }
+            }
+            return -1;
+        }
+};
