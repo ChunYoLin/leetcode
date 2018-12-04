755e99de735cdbd73f31366a837398d5f091d2c0
ChunYoLin <r04922108@csie.ntu.edu.tw>
2018-12-04 21:51:07 +0800

    update 475.heaters.cpp

---
+++ 475.heaters.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+        int findRadius(vector<int>& houses, vector<int>& heaters)
+        {
+            if (houses == heaters)
+                return 0;
+            if (houses.empty())
+                return 0;
+            int radius = 0;
+            sort(heaters.begin(), heaters.end());
+            for (auto &house : houses)
+            {
+                int distance = -1;
+                for (auto &heater : heaters)
+                {
+                    int dis = abs(heater-house);
+                    if (distance == -1)
+                        distance = dis;
+                    else if (distance > dis)
+                        distance = dis;
+                    else
+                        break;
+                }
+                radius = max(radius, distance);
+            }
+            return radius;
+        }
+};
