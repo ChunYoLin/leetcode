2b6956945a5163ef62b103f15f660b614dd75047
ChunYoLin <r04922108@csie.ntu.edu.tw>
2018-12-11 23:10:53 +0800

    update 338.counting-bits.cpp

--- 338.counting-bits.cpp
+++ 338.counting-bits.cpp
@@ -3,13 +3,9 @@ class Solution
     public:
         vector<int> countBits(int num)
         {
-            vector<int> ans(1, 0);
             vector<int> table(num+1, 0);
             for (int i = 1; i <= num; i++)
-            {
                 table[i] = (i&1) + table[i/2];
-                ans.push_back(table[i]);
-            }
-            return ans;
+            return table;
         }
 };
