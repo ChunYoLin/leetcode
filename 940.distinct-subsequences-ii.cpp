fd159dbbc4c39ed4dc85c953cc1e766db4a6adcb
ChunYoLin <r04922108@csie.ntu.edu.tw>
2019-03-04 21:59:45 +0800

    update 940.distinct-subsequences-ii.cpp

--- 940.distinct-subsequences-ii.cpp
+++ 940.distinct-subsequences-ii.cpp
@@ -5,15 +5,16 @@ class Solution
         {
             int mod = 1000000007;
             vector<int> dp(S.size()+1, 0);
-            map<char, int> last;
+            vector<int> last(26, 0);
             dp[0] = 1;
             for (int i = 1; i < S.size()+1; i++)
             {
                 dp[i] = dp[i-1]*2 % mod;
-                if (last.count(S[i-1]))
-                    dp[i] -= dp[last[S[i-1]] - 1];
+                char ch = S[i-1]-'a';
+                if (last.at(ch) != 0)
+                    dp[i] -= dp[last[ch] - 1];
                 dp[i] %= mod;
-                last[S[i-1]] = i;
+                last[ch] = i;
             } 
             return (((dp[S.size()] - 1) + mod) % mod);
         }
