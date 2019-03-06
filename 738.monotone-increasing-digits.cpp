850a32682f9bd2b9593950e437255c165ba79873
ChunYoLin <r04922108@csie.ntu.edu.tw>
2019-03-06 22:04:11 +0800

    update 738.monotone-increasing-digits.cpp

--- 738.monotone-increasing-digits.cpp
+++ 738.monotone-increasing-digits.cpp
@@ -4,17 +4,24 @@ class Solution
         int monotoneIncreasingDigits(int N)
         {
             string n_str = to_string(N);
-            int marker = n_str.size();
-            for (int i = n_str.size()-1; i > 0; i--)
+            int inversion = n_str.size();
+            for (int i = 0; i < n_str.size()-1; i++)
             {
-                if (n_str[i-1] > n_str[i])
+                if (n_str[i+1] < n_str[i])
                 {
-                    n_str[i-1]--;
-                    marker = i;
+                    inversion = i;
+                    while (n_str[inversion-1] == n_str[i])
+                        inversion--;
+                    break;
                 }
             }
-            for (int i = marker; i < n_str.size(); i++)
-                n_str[i] = '9';
+            for (int i = inversion; i < n_str.size(); i++)
+            {
+                if (i == inversion)
+                    n_str[i]--;
+                else
+                    n_str[i] = '9';
+            }
             return stoi(n_str);
         }
 };
