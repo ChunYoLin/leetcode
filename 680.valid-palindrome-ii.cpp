4984d93b6a7c72b9a2736472005cfc8313e96b0d
ChunYoLin <r04922108@csie.ntu.edu.tw>
2019-03-07 22:29:03 +0800

    update 680.valid-palindrome-ii.cpp

--- 680.valid-palindrome-ii.cpp
+++ 680.valid-palindrome-ii.cpp
@@ -3,7 +3,23 @@ class Solution
     public:
         bool validPalindrome(string s)
         {
-            return valid(s, 0, s.size()-1, false);
+            for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+                if (s[i] != s[j])
+                {
+                    int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
+                    while (i1 < j1 && s[i1] == s[j1])
+                    {
+                        i1++;
+                        j1--;
+                    };
+                    while (i2 < j2 && s[i2] == s[j2])
+                    {
+                        i2++;
+                        j2--;
+                    };
+                    return i1 >= j1 || i2 >= j2;
+                }
+            return true;
         }
 
         bool valid(const string& s, int head, int end, bool has_del)
