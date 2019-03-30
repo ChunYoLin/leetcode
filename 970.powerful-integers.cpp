e230eb8690a0d0aac7281003750ef520241cb2c7
ChunYoLin <r04922108@csie.ntu.edu.tw>
2019-03-30 16:32:19 +0800

    update 970.powerful-integers.cpp

--- 970.powerful-integers.cpp
+++ 970.powerful-integers.cpp
@@ -3,35 +3,29 @@ class Solution
     public:
         vector<int> powerfulIntegers(int x, int y, int bound)
         {
-            vector<int> x_list{1};
-            vector<int> y_list{1};
+            int i = 0, j = 0;
             if (x > 1)
             {
-                x_list.push_back(x);
-                int _x = x;
-                while (_x < bound)
-                {
-                    _x *= x;
-                    x_list.push_back(_x);
-                }
+                while (pow(x, i) < bound)
+                    i++;
             }
-
             if (y > 1)
             {
-                y_list.push_back(y);
-                int _y = y;
-                while (_y < bound)
-                {
-                    _y *= y;
-                    y_list.push_back(_y);
-                }
+                while (pow(y, j) < bound)
+                    j++;
             }
 
             vector<int> ans;
-            for (auto x : x_list)
-                for (auto y : y_list)
-                    if (x+y <= bound && find(ans.begin(), ans.end(), x+y) == ans.end())
-                        ans.push_back(x+y);
+
+            if (x == 1) i = 1;
+            if (y == 1) j = 1;
+            for (int _i = 0; _i < i; _i++)
+                for (int _j = 0; _j < j; _j++)
+                {
+                    int a = pow(x, _i) + pow(y, _j);
+                    if (a <= bound && find(ans.begin(), ans.end(), a) == ans.end())
+                        ans.push_back(a);
+                }
             return ans;
         }
 };
