7808d1f508ee267518b6165e9a0c3dfa0c5367b0
ChunYoLin <r04922108@csie.ntu.edu.tw>
2019-04-14 17:31:50 +0900

    update 423.reconstruct-original-digits-from-english.cpp

--- 423.reconstruct-original-digits-from-english.cpp
+++ 423.reconstruct-original-digits-from-english.cpp
@@ -3,31 +3,23 @@ class Solution
     public:
         string originalDigits(string s)
         {
-            vector<int> counts(10, 0);
-            for (auto &c : s)
-            {
-                if (c == 'z') counts[0]++;
-                else if (c == 'o') counts[1]++;
-                else if (c == 'w') counts[2]++;
-                else if (c == 'r') counts[3]++;
-                else if (c == 'u') counts[4]++;
-                else if (c == 'f') counts[5]++;
-                else if (c == 'x') counts[6]++;
-                else if (c == 's') counts[7]++;
-                else if (c == 'g') counts[8]++;
-                else if (c == 'i') counts[9]++;
-            }
-
-            counts[1] = counts[1] - counts[0] - counts[2] - counts[4];
-            counts[3] = counts[3] - counts[0] - counts[4];
-            counts[5] = counts[5] - counts[4];
-            counts[7] = counts[7] - counts[6];
-            counts[9] = counts[9] - counts[5] - counts[6] - counts[8];
-
+            vector<int> mp(26, 0), nums(10, 0);
+            for (char c:s) mp[c-'a']++;
+            nums[0] = mp['z'-'a'];
+            nums[2] = mp['w'-'a'];
+            nums[4] = mp['u'-'a'];
+            nums[1] = mp['o'-'a']-nums[0]-nums[2]-nums[4];
+            nums[3] = mp['r'-'a']-nums[0]-nums[4];
+            nums[6] = mp['x'-'a'];
+            nums[7] = mp['s'-'a']-nums[6];
+            nums[8] = mp['g'-'a'];
+            nums[5] = mp['v'-'a']-nums[7];
+            nums[9] = mp['i'-'a']-nums[5]-nums[6]-nums[8];
             string ans;
-            for (int i = 0; i <= 9; ++i)
-                while (counts[i]--)
-                    ans += to_string(i);
+            for (int i = 0; i < 10; i++)
+            {
+                ans += string(nums[i], '0'+i);
+            }
             return ans;
         }
 };
