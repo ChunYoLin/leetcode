7b637be75a3f717e13cde9534b3270750451e8de
ChunYoLin <r04922108@csie.ntu.edu.tw>
2019-04-26 21:12:13 +0800

    update 455.assign-cookies.cpp

--- 455.assign-cookies.cpp
+++ 455.assign-cookies.cpp
@@ -7,18 +7,16 @@ class Solution
             sort(s.begin(), s.end());
             
             int g_ptr = 0, s_ptr = 0;
-            int ans = 0;
             while (g_ptr < g.size() && s_ptr < s.size())
             {
                 if (g[g_ptr] <= s[s_ptr])
                 {
                     g_ptr++;
                     s_ptr++;
-                    ans += 1;
                 }
                 else if (s[s_ptr] < g[g_ptr])
                     s_ptr++;
             }
-            return ans;
+            return g_ptr;
         }
 };
